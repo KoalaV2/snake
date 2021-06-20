16a66ddc5f86521a7455023042acb3b677044de2
KoalaV2 <theo.johanson@gmail.com>
2021-06-20 12:45:29 +0200

    Able to eat to earn points now

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ import random
 pygame.init()
 
 white = (255,255,255)
-font_style = pygame.font.SysFont(None, 50)
+red = (204,35,28)
+
 class Map:
    def __init__(self):
         self.W = 800
         self.H = 800
         self.foreground = (235, 219, 178)
         self.background = (39, 39, 39)
+        self.font_style = pygame.font.SysFont(None, 50)
         self.snake_block = 10
-
-        self.foodx = round(random.randrange(0, self.W - self.snake_block))
-        self.foody = round(random.randrange(0, self.H - self.snake_block))
-
         self.display = pygame.display.set_mode((self.W,self.H))
         self.display.fill(self.background)
         pygame.display.set_caption('Snääääk')
@@ -32,38 +30,45 @@ class Snake:
         self.x_tmp = 0
         self.y_tmp = 0
         self.dead = False
+        self.foodx = round(random.randrange(0, self.x - 10) / 10) * 10
+        self.foody = round(random.randrange(0, self.y - 10) / 10) * 10
+
+
 
 def main():
     clock = pygame.time.Clock()
     game_map = Map()
     snake = Snake(800,800)
+
     while snake.dead == False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                 snake.dead = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_h:
+                if event.key == pygame.K_h or event.key == pygame.K_LEFT:
                     snake.x_tmp = -10
                     snake.y_tmp = 0
-                elif event.key == pygame.K_l:
+                elif event.key == pygame.K_l or event.key == pygame.K_RIGHT:
                     snake.x_tmp = 10
                     snake.y_tmp = 0
-                elif event.key == pygame.K_k:
+                elif event.key == pygame.K_k or event.key == pygame.K_UP:
                     snake.y_tmp = -10
                     snake.x_tmp = 0
-                elif event.key == pygame.K_j:
+                elif event.key == pygame.K_j or event.key == pygame.K_DOWN:
                     snake.y_tmp = 10
                     snake.x_tmp = 0
         print(event)
+
         if snake.x >= game_map.W or snake.x < 0 or snake.y >= game_map.H or snake.y < 0:
             snake.dead = True
+
         snake.x += snake.x_tmp
         snake.y += snake.y_tmp
+        pygame.draw.rect(game_map.display, red, [snake.foodx, snake.foody, game_map.snake_block, game_map.snake_block])
         pygame.draw.rect(game_map.display, game_map.foreground, [snake.x, snake.y, 10, 10])
-        pygame.draw.rect(game_map.display, game_map.foreground, [game_map.foodx, game_map.foodx, game_map.snake_block, game_map.snake_block])
-        print(round(snake.x))
-        print(round(game_map.foodx))
-        if round(snake.x) == game_map.foodx and round(snake.y) == game_map.foody:
+        print(snake.x)
+        print(snake.foodx)
+        if snake.x == snake.foodx and snake.y == snake.foody:
             print("Eaten")
 
         pygame.display.update()
